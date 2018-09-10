4d06bf4b1cb30e7a15037f4a901713c5b74afbf8
Piotr Palucki <piotrek.palucki@gmail.com>
2018-09-10 14:36:01 +0200

    Added GlosbeTranslator class

--- main.py
+++ main.py
@@ -7,6 +7,59 @@
 import os
 import random
 from pymongo import MongoClient
+import requests
+
+
+class EnglishPhrase:
+    """Class representing single entry in dictionary"""
+    text = ""
+    meanings = []
+
+    def __init__(self, text):
+        self.text = text
+        
+    def __str__(self):
+        output = "Word: " + self.text + "\n";
+        for idx, meaning in enumerate(self.meanings):
+            output += "%3d. %s\n" % (idx, meaning)
+            
+        return output
+        
+    def show(self):        
+        print("Word:", self.text)
+        print("Meanings:")        
+        for idx, meaning in enumerate(self.meanings):
+            print("%3d. %s" % (idx, meaning))
+            
+    def add_meaning(self, phrase):
+        self.meanings.append(phrase)
+
+class GlosbeTranslator:
+    """Client class of Glosbe translator API"""
+    base_url = "https://glosbe.com/gapi/translate"
+    payload = {'from': 'eng', 'dest': 'pol', "format" : "json", "phrase" : "", "pretty" : "true"}
+    
+    def translate(self, word):
+        self.payload["phrase"] = word
+        raw_resp = requests.get(self.base_url, params=self.payload)
+        #print (raw_resp.text)
+        resp = raw_resp.json()
+
+        if resp["result"] == "ok":
+            print("Successfully translated")
+            
+            phrase = EnglishPhrase(word)
+            contents = resp["tuc"]
+            
+            for entry in contents:
+                if "phrase" in entry and "text" in entry["phrase"]:
+                    meaning = entry["phrase"]["text"]
+                    #print(meaning)
+                    phrase.add_meaning(meaning)
+
+            return phrase
+        else:
+            print("Request failed!!!")
 
 def init_db():
     client = MongoClient('mongodb://localhost:27017/')
@@ -31,34 +84,14 @@ def record_exists(db, table, eng):
     else:
         return False
 
+
+
+
 mydb = init_db()
-#mydb.phrases.drop()
-
-#add_to_db(mydb, "phrases", record1)
-#add_to_db(mydb, "phrases", record2)
-
-#iterate_over_db(mydb, "phrases")
-
-#one_found = mydb.phrases.find_one({"english" : "delve into"})
-#all_found = mydb.phrases.find({"english" : "delve into"})
-
-#print(one_found["polish"])
-
-#print("Found %d result(s)" % all_found.count())
-
-#for one in all_found:
-    #print(one)
-
-class Phrase:
-    """Class representing single entry in dictionary"""
-    english = ""
-    polish = ""
-
-    def __init__(self, eng, pl):
-        self.english = eng
-        self.polish = pl
-
 decision = 1
+word = "speaker"
+translator = GlosbeTranslator()
+phrase = translator.translate(word)
 
 while True:
     os.system("clear")
@@ -112,52 +145,4 @@ while True:
     elif decision == "0":
         os.system("clear")
         break
-
-
-
-
-import requests
-
-def translate_phrase(phrase, src, dst):
-    translated = "nothing"
-
-    return translated
-
-#https://glosbe.com/gapi/translate?from=pol&dest=eng&format=json&phrase=witaj&pretty=true
-
-#print ("translating...", translate_phrase("coś", "pol", "eng"))
-
-payload = {'from': 'eng', 'dest': 'pol', "format" : "json", "phrase" : "speaker", "pretty" : "true"}
-resp = requests.get('https://glosbe.com/gapi/translate', params=payload).json()
-
-if resp["result"] == "ok":
-    print("Success")
-    contents = resp["tuc"]
-    print(contents)
-   
-    translation = contents["phrase"]["text"]
-    meaning_from = contents["meanings"][0]["text"]
-    meaning_dest = contents["meanings"][1]["text"]
-   
-    print("\n",translation)
-    print("\n",meaning_from)
-    print("\n",meaning_dest)
-
-
-   
-   
-   
-    # meanings = resp["tuc"]
-    # print("samochód", "means", meanings[0]["text"])
-   
-   
-# print(r.url)
-# print(r.text)
-# print(r.json())
-
-# if r.text["result"] == "ok":
-
-    # print(r.text)
-#r = requests.get("https://glosbe.com/gapi/translate?from=pol&dest=eng&format=json&phrase=witaj&pretty=true")
-#print("received response")
 
